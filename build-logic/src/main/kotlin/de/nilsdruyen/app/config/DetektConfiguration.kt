abd51d689375830c1695e09cce08eecb7da4e0f6
Nils Druyen <info@nilsdruyen.de>
2026-04-22 22:46:58 +0200

    Fix code review findings

--- build-logic/src/main/kotlin/de/nilsdruyen/app/config/DetektConfiguration.kt
+++ build-logic/src/main/kotlin/de/nilsdruyen/app/config/DetektConfiguration.kt
@@ -14,36 +14,19 @@ internal fun Project.applyDetekt() {
     val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
     pluginManager.apply(DetektPlugin::class.java)
     extensions.configure<DetektExtension> {
-        toolVersion.set("2.0.0-alpha.2")
-        ignoreFailures.set(true)
+        toolVersion.set(libs.findVersion("detekt").get().toString())
+        ignoreFailures.set(false)
         parallel.set(true)
         source.setFrom(files("src/main/kotlin"))
         config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
         buildUponDefaultConfig.set(true)
     }
-//    tasks.withType<Detekt>().configureEach {
-//        jvmTarget.set(JavaVersion.VERSION_25.toString())
-//        reports {
-//            checkstyle {
-//                required.set(true)
-//                outputLocation.set(file("${layout.buildDirectory}/reports/detekt/detekt.xml"))
-//            }
-//            html.required.set(false)
-//            sarif.required.set(false)
-//            markdown.required.set(false)
-//        }
-////        dependencies {
-////            add("detektPlugins", libs.findLibrary("detekt.composeRules").get())
-////            add("detektPlugins", libs.findLibrary("detekt.ktlintWrapper").get())
-////        }
-//    }
 }
 
-//
 internal fun Project.applyDetektFormatting() {
     val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
     pluginManager.apply(DetektPlugin::class)
-    //
+
     fun Detekt.configure(enableAutoCorrect: Boolean) {
         description = "Run detekt ktlint wrapper"
         parallel.set(true)
@@ -53,23 +36,6 @@ internal fun Project.applyDetektFormatting() {
         disableDefaultRuleSets.set(true)
         autoCorrect.set(enableAutoCorrect)
         include("*.kts", "src/*/kotlin/**/*.kt")
-//        reports {
-//            checkstyle {
-//                required.set(true)
-//                outputLocation.set(file("${layout.buildDirectory}/reports/detekt/detektFormatting.xml"))
-//            }
-//            sarif.required.set(true)
-//            html.required.set(false)
-//            sarif.required.set(false)
-//            markdown.required.set(false)
-//        }
-//        if (project == rootProject) {
-//            include(listOf("*.kts", "build-logic/*.kts", "build-logic/src/*/kotlin/**/*.kts"))
-//            exclude("build-logic/build/")
-//        } else {
-//            include(listOf("src/*/kotlin/**/*.kt", "*.kts"))
-//            exclude("build/")
-//        }
         dependencies.add("detektPlugins", libs.findLibrary("detekt.composeRules").get())
         dependencies.add("detektPlugins", libs.findLibrary("detekt.ktlintWrapper").get())
     }
